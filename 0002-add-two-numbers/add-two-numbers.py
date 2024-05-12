1ceaae1bbbff804267ed77223f44f12a2f8dbb43
GitHub Action <action@github.com>
2024-05-12 18:38:45 +0000

    Automatically rename directories

--- 0002-add-two-numbers/add-two-numbers.py
+++ 0002-add-two-numbers/add-two-numbers.py
@@ -1,46 +1,39 @@
 # Definition for singly-linked list.
-class ListNode:
-    def __init__(self, val=-1, next=None):
-        self.val = val
-        self.next = next
-    
-    def add(self, val):
-        cur = self.val
-        if cur == -1:
-            self.val = val
-            return
-        cur = self
-        while cur.next is not None:
-            cur = cur.next
-        cur.next = ListNode(val)
-        return
-
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        cur_left = l1
-        cur_right = l2
-        buffer = 0
-        result = ListNode()
-        empty = True
-        while cur_left is not None or cur_right is not None:
-            if cur_left is not None:
-                buffer += cur_left.val
-            if cur_right is not None:
-                buffer += cur_right.val
-            if not empty:
-                result.add(buffer % 10)
-            else:
-                result.val = buffer % 10
-                empty = False
-            buffer //= 10
-            if cur_left is not None:
-                cur_left = cur_left.next
-            if cur_right is not None:
-                cur_right = cur_right.next
-        if buffer > 0:
-            if empty:
-                result.val = buffer
-                empty = False
-            else:
-                result.add(buffer)
-        return result
+        left = l1
+        right = l2
+        overflow = 0
+        while left.next is not None and right.next is not None:
+            cur_val = left.val + right.val + overflow
+            left.val, overflow = cur_val % 10, cur_val // 10
+            left = left.next
+            right = right.next
+        cur_val = left.val + right.val + overflow
+        left.val, overflow = cur_val % 10, cur_val // 10
+        if left.next is None and right.next is None:
+            if overflow != 0:
+                left.next = ListNode(overflow, None)
+            return l1
+        if right.next is None:
+            left = left.next
+        else:
+            left.next = right.next
+            left = left.next
+
+        while overflow > 0 and left.next is not None:
+            cur_val = left.val + overflow
+            left.val, overflow = cur_val % 10, cur_val // 10
+            left = left.next
+        cur_val = left.val + overflow
+        left.val, overflow = cur_val % 10, cur_val // 10
+        if overflow > 0:
+            left.next = ListNode(overflow, None)
+        return l1
+             
+
+
