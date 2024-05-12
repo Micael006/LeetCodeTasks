06fd38653675c9682c7f9bdb16c2952804385bae
GitHub Action <action@github.com>
2024-05-12 18:47:19 +0000

    Automatically rename directories

--- 0003-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -1,17 +1,15 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
+        if s == "":
+            return 0
         alphabet = dict()
+        start, end = 0, 0
         max_len = 0
-        cur_len = 0
-        left = 0
         for i in range(len(s)):
-            if alphabet.get(s[i]) is None:
-                alphabet[s[i]] = i
-                cur_len += 1
-            else:
-                max_len = max(max_len, cur_len)
-                left = max(left, alphabet[s[i]] + 1)
-                cur_len = i - left + 1
-                alphabet[s[i]] = i
-        max_len = max(max_len, cur_len)
+            if alphabet.get(s[i]) is not None:
+                max_len = max(end - start + 1, max_len)
+                start = max(alphabet[s[i]] + 1, start)
+            alphabet[s[i]] = i
+            end = i
+        max_len = max(end - start + 1, max_len)
         return max_len
