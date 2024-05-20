16e04c94a9b4902f2a40786cfb13b628deb1b679
Micael006 <58003787+Micael006@users.noreply.github.com>
2024-05-20 11:52:52 +0300

    Time: 92 ms (15.01%) | Memory: 16.5 MB (84.04%) - LeetSync

--- 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.py
+++ 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.py
@@ -2,9 +2,14 @@ from functools import reduce
 class Solution:
     def subsetXORSum(self, nums: List[int]) -> int:
         count = 0
-        for i in range(1, 2**len(nums)):
-            helper = bin(i)[2:][::-1]
-            arr = [nums[x] for x in range(len(helper)) if helper[x] == '1']
-            count += reduce(lambda x, y: x ^ y, arr, 0)
-            print(helper, arr)
+        n = len(nums)
+        for i in range(1, 2**n):
+            xor = 0
+            for j in range(n):
+                if i & (1 << j):
+                    xor ^= nums[j]
+            count += xor
+            #arr = [nums[x] for x in range(len(helper)) if helper[x] == '1']
+            #count += reduce(lambda x, y: x ^ y, arr, 0)
+            #print(helper, arr)
         return count
