f3036111a15efd5acf927594f65efed238353755
Micael006 <58003787+Micael006@users.noreply.github.com>
2024-05-30 10:31:46 +0300

    Time: 48 ms (64.37%) | Memory: 16.6 MB (66.09%) - LeetSync

--- 1553-count-triplets-that-can-form-two-arrays-of-equal-xor/count-triplets-that-can-form-two-arrays-of-equal-xor.py
+++ 1553-count-triplets-that-can-form-two-arrays-of-equal-xor/count-triplets-that-can-form-two-arrays-of-equal-xor.py
@@ -1,27 +1,15 @@
 class Solution:
     def countTriplets(self, arr: List[int]) -> int:
-        table = dict()
-        for i in range(len(arr)):
-            helper = arr[i]
-            table[(i, i)] = helper
-            for j in range(i + 1, len(arr)):
-                helper ^= arr[j]
-                table[(i, j)] = helper
+        n = len(arr)
+        prefix = [0] * (n + 1)
 
+        for i in range(n):
+            prefix[i + 1] = prefix[i] ^ arr[i]
+        
         count = 0
-        for i in range(len(arr) - 1):
-            for j in range(i + 1, len(arr)):
-                for k in range(j, len(arr)):
-                    a = table[(i, j - 1)]
-                    b = table[(j, k)]
-                    if a == b:
-                        count += 1
-                    '''a = arr[i]
-                    for z in range(i + 1, j):
-                        a ^= arr[z]
-                    b = arr[j]
-                    for z in range(j + 1, k + 1):
-                        b ^= arr[z]
-                    if a==b:
-                        count += 1'''
+        for i in range(n):
+            for k in range(i + 1, n):
+                if prefix[i] == prefix[k + 1]:
+                    count += (k - i)
+
         return count
