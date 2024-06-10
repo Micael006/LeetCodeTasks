5c4e21c3c49dfcfce129669ee06960df6bf3be31
Micael006 <58003787+Micael006@users.noreply.github.com>
2024-06-10 10:01:33 +0300

    Time: 42 ms (37.58%) | Memory: 16.5 MB (77.64%) - LeetSync

--- 1137-height-checker/height-checker.py
+++ 1137-height-checker/height-checker.py
@@ -1,7 +1,12 @@
 class Solution:
     def heightChecker(self, heights: List[int]) -> int:
-        expected = heights[:]
-        expected.sort()
+        mn, mx = min(heights), max(heights)
+        keys = [0] * (mx - mn + 1)
+        for i in range(len(heights)):
+            keys[heights[i] - mn] += 1
+        expected = []
+        for i in range(len(keys)):
+            expected += [mn + i] * keys[i]
         count = 0
         for i in range(len(heights)):
             count += expected[i] != heights[i]
