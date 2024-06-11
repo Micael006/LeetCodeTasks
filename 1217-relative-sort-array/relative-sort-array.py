0f34a1484e45980b980999ea4c299220986adeb1
Micael006 <58003787+Micael006@users.noreply.github.com>
2024-06-11 10:06:45 +0300

    Time: 41 ms (57.03%) | Memory: 16.6 MB (67.06%) - LeetSync

--- 1217-relative-sort-array/relative-sort-array.py
+++ 1217-relative-sort-array/relative-sort-array.py
@@ -1,17 +1,14 @@
 class Solution:
     def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
         keys = [0] * 1001
-        for elem in arr1:
-            keys[elem] += 1
-        for elem in arr2:
-            keys[elem] *= -1
         start_arr = []
         end_arr = []
-        for i in range(len(keys)):
-            if keys[i] >= 0:
-                end_arr += [i] * keys[i]
+        for elem in arr1:
+            keys[elem] += 1
         for i in range(len(arr2)):
-            start_arr += [arr2[i]] * -keys[arr2[i]]
-        #print(start_arr)
-        #print(end_arr)
+            start_arr += [arr2[i]] * keys[arr2[i]]
+            keys[arr2[i]] = 0
+        for i in range(len(keys)):
+            end_arr += [i] * keys[i]
+
         return start_arr + end_arr
