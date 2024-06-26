b48a90b6ec11404fe304b7503388bae03ad91a84
GitHub Action <action@github.com>
2024-06-26 15:46:29 +0000

    Automatically rename directories

--- 0001-two-sum/two-sum.py
+++ 0001-two-sum/two-sum.py
@@ -2,6 +2,6 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         check = dict()
         for i in range(len(nums)):
-            if check.get(target - nums[i]) is not None:
-                return([check[target - nums[i]], i])
+            if check.get(target - nums[i], None) is not None:
+                return [i, check[target - nums[i]]]
             check[nums[i]] = i
--- 1-two-sum/two-sum.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        check = dict()
-        for i in range(len(nums)):
-            if check.get(target - nums[i], None) is not None:
-                return [i, check[target - nums[i]]]
-            check[nums[i]] = i
