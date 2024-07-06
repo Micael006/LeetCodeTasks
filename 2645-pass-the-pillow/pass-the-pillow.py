70e162651f1a27d3abf1a3de75312bf1680e775b
Micael006 <58003787+Micael006@users.noreply.github.com>
2024-07-06 11:02:11 +0300

    Time: 38 ms (35.95%) | Memory: 16.5 MB (77.72%) - LeetSync

--- 2645-pass-the-pillow/pass-the-pillow.py
+++ 2645-pass-the-pillow/pass-the-pillow.py
@@ -2,7 +2,7 @@ class Solution:
     def passThePillow(self, n: int, time: int) -> int:
         rotation = time // (n - 1)
         if rotation % 2 == 0:
-            return (time - rotation * (n - 1)) % n + 1
+            return time % (n - 1) + 1
         else:
-            return (n - 1) - ((time - rotation * (n - 1)) % n) + 1
+            return n - time % (n - 1)
         
