b2c5846232c56587b2514dafcade804754c23e41
GitHub Action <action@github.com>
2024-07-13 17:40:25 +0000

    Automatically rename directories

--- 0782-jewels-and-stones/jewels-and-stones.py
+++ 0782-jewels-and-stones/jewels-and-stones.py
@@ -1,3 +1,11 @@
 class Solution:
     def numJewelsInStones(self, jewels: str, stones: str) -> int:
-        return len([x for x in stones if x in jewels])
+        my_dict = dict()
+        for i in range(len(jewels)):
+            my_dict[jewels[i]] = 0
+        
+        counter = 0
+        for i in range(len(stones)):
+            if stones[i] in my_dict:
+                counter += 1
+        return counter
