2a16d89b1cb25565e5bf249dceb2e069dd0d2222
GitHub Action <action@github.com>
2024-07-13 09:59:10 +0000

    Automatically rename directories

--- 0167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -1,15 +1,6 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        l, r = 0, 1
-        while l < r and r < len(numbers) - 1:
-            if numbers[l] + numbers[r] == target:
-                return [l + 1, r + 1]
-            elif numbers[l] + numbers[r] < target:
-                r += 1
-            else:
-                r -= 1
-                break
-        
+        l, r = 0, len(numbers) - 1
         while l < r:
             if numbers[l] + numbers[r] == target:
                 return [l + 1, r + 1]
