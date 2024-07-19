b285cc9d7737117902414ac1e821957f73d58291
Micael006 <58003787+Micael006@users.noreply.github.com>
2024-07-19 11:36:17 +0300

    Time: 116 ms (34.38%) | Memory: 16.9 MB (15.89%) - LeetSync

--- 1496-lucky-numbers-in-a-matrix/lucky-numbers-in-a-matrix.py
+++ 1496-lucky-numbers-in-a-matrix/lucky-numbers-in-a-matrix.py
@@ -2,11 +2,17 @@ class Solution:
     def luckyNumbers (self, matrix: List[List[int]]) -> List[int]:
         ans = []
         for i in range(len(matrix)):
-            cur_min = min(matrix[i])
-            j = matrix[i].index(cur_min)
-            cur_max = max([matrix[z][j] for z in range(len(matrix))])
-            
-            if cur_max == cur_min:
-                ans.append(cur_min)
+            cur_min = [max(matrix[i]), -1]
+            for j in range(len(matrix[i])):
+                if matrix[i][j] < cur_min[0]:
+                    cur_min = [matrix[i][j], j]
+
+            cur_max = [-1, -1]
+            for j in range(len(matrix)):
+                if matrix[j][cur_min[1]] > cur_max[0]:
+                    cur_max = [matrix[j][cur_min[1]], j]
+
+            if i == cur_max[1]:
+                return [cur_min[0]]
         
-        return ans
+        return []
