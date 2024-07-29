1211832b0ca18d1e9b2215b9a5630fabf8734742
GitHub Action <action@github.com>
2024-07-29 09:31:07 +0000

    Automatically rename directories

--- 0056-merge-intervals/merge-intervals.py
+++ 0056-merge-intervals/merge-intervals.py
@@ -2,13 +2,16 @@ class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         intervals.sort()
         start, end = intervals[0]
-        print(start, end)
         answer = []
+
         for i in range(1, len(intervals)):
-            if intervals[i][0] <= end:
-                end = max(end, intervals[i][1])
+            cur_start, cur_end = intervals[i]
+            if cur_start <= end:
+                end = max(end, cur_end)
             else:
                 answer.append([start, end])
-                start, end = intervals[i]
+                start, end = cur_start, cur_end
+        
         answer.append([start, end])
+
         return answer
