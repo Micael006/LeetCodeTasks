38a90e7a4ac81d87d6a708157a632d094c64f3c3
Micael006 <58003787+Micael006@users.noreply.github.com>
2024-08-06 22:58:47 +0300

    Time: 213 ms (36.45%) | Memory: 17.6 MB (73.38%) - LeetSync

--- 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.py
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.py
@@ -3,17 +3,11 @@ class Solution:
         alphabet = [0] * 26
         for sym in word:
             alphabet[ord(sym) - ord('a')] += 1
-        
-        for i in range(len(alphabet)):
-            alphabet[i] = [alphabet[i], chr(i + ord('a'))]
         alphabet.sort(reverse=True)
 
-        taps = dict()
-        for i in range(len(alphabet)):
-            taps[alphabet[i][1]] = (i // 8) + 1
-        
         answer = 0
-        for elem in word:
-            answer += taps[elem]
+        for i in range(len(alphabet)):
+            answer += alphabet[i] * ((i // 8) + 1)
+        
         return answer
         
