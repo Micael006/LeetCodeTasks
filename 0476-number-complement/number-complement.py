499229861a9215d9db3d34186a2dea4e2df31ddb
GitHub Action <action@github.com>
2024-08-22 20:47:05 +0000

    Automatically rename directories

--- 0476-number-complement/number-complement.py
+++ 0476-number-complement/number-complement.py
@@ -1,3 +1,4 @@
 class Solution:
     def findComplement(self, num: int) -> int:
-        return int(''.join([['0', '1'][1 - int(x)] for x in list(str(bin(num))[2:])]), 2)
+        #return int(''.join([['0', '1'][1 - int(x)] for x in list(str(bin(num))[2:])]), 2)
+        return 2**len(bin(num)[2:]) - 1 - num
